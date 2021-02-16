eeeafa758711e1b1c18c571e6ce49122efa1a252
saudhasan762 <azanali152@gmail.com>
2021-02-16 23:36:42 +0530

    Adding a file to get Objects

---
+++ CompanyEmployeeWage.java
@@ -0,0 +1,15 @@
+
+public class CompanyEmployeeWage {
+	public String company;
+	public int empRatePerHour;
+	public int numOfWorkingDays;
+	public int maxHoursPerMonth;
+	
+	public CompanyEmployeeWage(String company, int empRatePerHour, int numOfWorkingDays,int maxHoursPerMonth) {
+		this.company = company;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+
+}
+}
